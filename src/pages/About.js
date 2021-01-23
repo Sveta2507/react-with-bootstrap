c3c66367a63b2ebec701f937d97ad0d8d4fb157d
Sveta2507 <dnsdns.dn@googlemail.com>
2021-01-23 12:54:06 +0200

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Jumbo from "../components/Jumbotron";
 
 function About() {
-  return <h2>About</h2>;
+  return (
+    <>
+      <h2>About</h2>
+      <Jumbo />
+    </>
+  );
 }
 
 export default About;
